bc196684e324288384cbfc89b1373c264587c494
Maksim <maksim-popov-1996@mail.ru>
2020-05-10 15:38:24 +0300

    Sending letter

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -3,8 +3,12 @@ import React, { Component } from 'react'
 export default class Contacts extends Component {
     render() {
         return (
-            <div>
-                Contacts
+            <div id="contacts">
+                <form action="mailto:maksim-popov-1996@mail.ru" method="get" target="_blank">
+                    <input type="text" name="subject" placeholder="ВВедите тему" />
+                    <textarea name="body"></textarea>
+                    <input type="submit" value="Create email"></input>
+                </form> 
             </div>
         )
     }
